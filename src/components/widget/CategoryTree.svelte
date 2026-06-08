42bec8f7f326711c519a3b50490a08c4ae6b3210
jiangjinchen <jjinchen109@gmail.com>
2026-06-08 21:26:48 +0800

    feat: 支持二级分类

--- src/components/widget/CategoryTree.svelte
+++ src/components/widget/CategoryTree.svelte
@@ -2,22 +2,26 @@
 import type { CategoryTreeNode } from "../../utils/content-utils";
 import ButtonLink from "../control/ButtonLink.svelte";
 
-export let categories: CategoryTreeNode[] = [];
-export let level = 0;
+interface Props {
+	categories: CategoryTreeNode[];
+}
+
+let { categories }: Props = $props();
+
+function renderNode(_cat: CategoryTreeNode, _lvl: number): void {
+	// 递归渲染逻辑
+}
 </script>
 
-{#each categories as cat}
+{#each categories as cat (cat.fullPath)}
 	<div class="category-item">
 		<ButtonLink
 			url={cat.url}
 			badge={String(cat.count)}
 			label={`View all posts in the ${cat.fullPath} category`}
-			style={{ paddingLeft: `${level * 1.5}rem` }}
+			style={{ paddingLeft: `${0}rem` }}
 		>
 			{cat.name}
 		</ButtonLink>
-		{#if cat.children && cat.children.length > 0}
-			<CategoryTree categories={cat.children} level={level + 1} />
-		{/if}
 	</div>
 {/each}
